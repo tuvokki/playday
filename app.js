d391d283a6bcb8d92622363d52cfc46ff24bcc9c
Wouter Roosendaal <tuvokki@gmail.com>
2017-05-10 09:57:58 +0200

    Update test and other stuffs

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+app.set('port', process.env.PORT || 4000);
 
-app.get('/', function(req, res) {
+app.get('/yolo', function(req, res) {
     res.send("Hello Yolo!");
 });
 
-app.listen(4000);
+app.get('/', function(req, res) {
+    res.send("Home");
+});
+
+console.log('Sarting server on http://localhost:' + app.get('port'))
+app.listen(app.get('port'));
--- test/test.spec.js
+++ test/test.spec.js
@@ -2,8 +2,8 @@ var supertest = require("supertest");
 var should = require("should");
 
 // This agent refers to PORT where the program is running.
-
-var server = supertest.agent("http://localhost:4000");
+var PORT = process.env.PORT || 4000;
+var server = supertest.agent("http://localhost:" + PORT);
 
 // UNIT test begin
 
@@ -19,6 +19,21 @@ describe("SAMPLE unit test", function() {
             .end(function(err, res) {
                 // HTTP status should be 200
                 res.status.should.equal(200);
+                done();
+            });
+    });
+
+    // #2 should return yolo page
+    it("should return yolo page", function(done) {
+        // calling yolo page
+        server
+            .get("/yolo")
+            .expect("Content-type", /text/)
+            .expect(200) // THis is HTTP response
+            .end(function(err, res) {
+                // HTTP status should be 200
+                res.status.should.equal(200);
+                res.text.should.equal("Hello Yolo!");
                 done();
             });
     });
